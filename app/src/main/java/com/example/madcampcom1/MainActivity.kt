d493e50e46bb87aa04ea16baa24e7ed65d651378
SungyeopJeong <syeopj4a@gmail.com>
2023-12-28 17:49:17 +0900

    [Edit] Move TabView to compose folder

--- app/src/main/java/com/example/madcampcom1/MainActivity.kt
+++ app/src/main/java/com/example/madcampcom1/MainActivity.kt
@@ -3,27 +3,12 @@ package com.example.madcampcom1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.madcampcom1.compose.TabView
 import com.example.madcampcom1.ui.theme.MadcampCom1Theme
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,64 +19,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    tabView()
+                    TabView()
                 }
             }
         }
     }
-}
-
-val pages = listOf("연락처", "이미지", "?")
-
-@OptIn(ExperimentalPagerApi::class)
-@Composable
-fun tabView() {
-    Column(
-        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center
-    ) {
-        val pagerState = com.google.accompanist.pager.rememberPagerState()
-        val coroutineScope = rememberCoroutineScope()
-
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                )
-            },
-        ) {
-            pages.forEachIndexed { index, title ->
-                Tab(
-                    content = {
-                        Text(
-                            text = title,
-                            modifier = Modifier.padding(24.dp)
-                        )
-                    },
-                    selected = pagerState.currentPage == index,
-                    onClick = {
-                        coroutineScope.launch {
-                            pagerState.scrollToPage(index)
-                        }
-                    },
-                    selectedContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            }
-        }
-
-        HorizontalPager(
-            count = pages.size,
-            state = pagerState,
-        ) { page ->
-            Text(
-                text = page.toString(), modifier = Modifier.wrapContentSize()
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-fun previewTabView() {
-    tabView()
 }
---
+++ app/src/main/java/com/example/madcampcom1/compose/TabView.kt
@@ -0,0 +1,78 @@
+package com.example.madcampcom1.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.TabRow
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+
+private val pages = listOf("연락처", "이미지", "?")
+
+@OptIn(ExperimentalPagerApi::class)
+@Preview
+@Composable
+fun TabView() {
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center
+    ) {
+        val pagerState = rememberPagerState()
+        TabRow(pagerState)
+
+        HorizontalPager(
+            count = pages.size,
+            state = pagerState,
+        ) { page ->
+            Text(
+                text = page.toString(), modifier = Modifier.wrapContentSize()
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun TabRow(pagerState: PagerState) {
+    val coroutineScope = rememberCoroutineScope()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
+        },
+    ) {
+        pages.forEachIndexed { index, title ->
+            Tab(
+                content = {
+                    Text(
+                        text = title,
+                        modifier = Modifier.padding(24.dp)
+                    )
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.scrollToPage(index)
+                    }
+                },
+                selectedContentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+    }
+}
