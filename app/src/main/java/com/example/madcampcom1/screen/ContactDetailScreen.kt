cb40d608ce86df8266c215349813f0057d1d6f65
SungyeopJeong <syeopj4a@gmail.com>
2024-01-03 15:00:23 +0900

    [Add] ContactDialog for setting defaultNumber

--- app/src/main/java/com/example/madcampcom1/screen/ContactDetailScreen.kt
+++ app/src/main/java/com/example/madcampcom1/screen/ContactDetailScreen.kt
@@ -1,5 +1,7 @@
 package com.example.madcampcom1.screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,11 +28,13 @@ import com.example.madcampcom1.ui.theme.Background
 import com.example.madcampcom1.ui.theme.Red
 import com.example.madcampcom1.viewModel.ContactViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContactDetailScreen(contactViewModel: ContactViewModel, onPop: () -> Unit) {
     val uiState by contactViewModel.uiState.collectAsState()
     val detailValue = uiState.detailValue!!
     val isDeleteDialogOpened = remember { mutableStateOf(false) }
+    val dialogValue = remember { mutableStateOf<String?>(null) }
 
     Scaffold(
         topBar = {
@@ -46,33 +51,45 @@ fun ContactDetailScreen(contactViewModel: ContactViewModel, onPop: () -> Unit) {
             }
         }, containerColor = Background
     ) {
-        LazyColumn(
-            modifier = Modifier.padding(it),
-            contentPadding = PaddingValues(top = 20.dp, bottom = 40.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
-        ) {
-            item {
-                ContactDetailInfo(
-                    name = detailValue.name, defaultNumber = detailValue.defaultNumber()
-                )
-            }
+        CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
+            LazyColumn(
+                modifier = Modifier.padding(it),
+                contentPadding = PaddingValues(top = 20.dp, bottom = 40.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                item {
+                    ContactDetailInfo(
+                        name = detailValue.name, defaultNumber = detailValue.defaultNumber()
+                    )
+                }
 
-            item {
-                ContactNumberList(detailValue.numbers) { number ->
-                    detailValue.copy(numbers = listOf(number) + (detailValue.numbers - number))
-                        .let { newDetailValue ->
-                            contactViewModel.updateContact(newDetailValue)
-                            contactViewModel.setDetailValue(newDetailValue)
-                        }
+                item {
+                    ContactNumberList(detailValue.numbers) { dialogValue.value = it }
                 }
             }
         }
 
-        if (isDeleteDialogOpened.value) ContactDialog("연락처를 삭제할까요?", onDismissRequest = {
-            isDeleteDialogOpened.value = false
-        }, confirmText = "삭제") {
+        if (isDeleteDialogOpened.value) ContactDialog(
+            message = "연락처를 삭제할까요?",
+            onDismissRequest = { isDeleteDialogOpened.value = false },
+            confirmText = "삭제"
+        ) {
             contactViewModel.removeContact(detailValue)
             onPop()
+        }
+
+        if (dialogValue.value != null) ContactDialog(
+            message = "기본 번호로 설정할까요?",
+            onDismissRequest = { dialogValue.value = null },
+            confirmText = "설정"
+        ) {
+            dialogValue.value!!.let { number ->
+                detailValue.copy(numbers = listOf(number) + (detailValue.numbers - number))
+                    .let { newDetailValue ->
+                        contactViewModel.updateContact(newDetailValue)
+                        contactViewModel.setDetailValue(newDetailValue)
+                    }
+            }
         }
     }
 }
